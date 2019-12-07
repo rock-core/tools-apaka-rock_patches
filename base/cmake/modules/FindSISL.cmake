d397ee35dc11e90523b274c13535131d3604393c
Thomas Roehr <thomas.roehr@dfki.de>
2019-12-07 23:17:09 +0100

    Add ROCK_INSTALL_DIR to path hint for sisl

---
+++ base/cmake/modules/FindSISL.cmake
@@ -0,0 +1,16 @@
+set(SISL_FOUND FALSE)
+
+if (NOT SISL_PREFIX)
+    set(SISL_PREFIX ${CMAKE_INSTALL_PREFIX} CACHE FILEPATH "the installation prefix for the SISL library")
+endif()
+
+find_path(SISL_INCLUDE_DIRS "sisl.h"
+    HINTS ${SISL_PREFIX}/include ${CMAKE_INSTALL_PREFIX}/include
+    PATHS @ROCK_INSTALL_DIR@/include)
+find_library(SISL_LIBRARIES
+    NAMES libsisl${CMAKE_SHARED_LIBRARY_SUFFIX} libsisl_opt${CMAKE_SHARED_LIBRARY_SUFFIX} libsisl.a libsisl_opt.a
+    HINTS ${SISL_PREFIX}/lib ${CMAKE_INSTALL_PREFIX}/lib
+    PATHS @ROCK_INSTALL_DIR@/lib)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SISL "SISL library not found, NURBS 3D curve wrappers won't be installed" SISL_INCLUDE_DIRS SISL_LIBRARIES)
